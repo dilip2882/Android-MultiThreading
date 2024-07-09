446703985a863b211fd06e064f51e9d04b095b4e
Dilip Patel <pateldilip2902@gmail.com>
2024-07-09 16:03:51 +0530

    Android-multithreading

---
+++ Android-multithreading/app/src/main/java/com/dilip/multithreading/common/ToolbarManipulator.java
@@ -0,0 +1,7 @@
+package com.dilip.multithreading.common;
+
+public interface ToolbarManipulator {
+    void setScreenTitle(String screenTitle);
+    void showUpButton();
+    void hideUpButton();
+}
